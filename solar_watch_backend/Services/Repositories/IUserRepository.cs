efe82dacbaa59e25af0f87c041f0cef5c96604e6
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-02 12:17:39 +0200

    updated IUserRepository.cs

--- solar_watch_backend/Services/Repositories/IUserRepository.cs
+++ solar_watch_backend/Services/Repositories/IUserRepository.cs
@@ -9,11 +9,12 @@ using solar_watch_backend.Models.Contracts;
 
 public interface IUserRepository
 {
-    Task<IEnumerable<User>> GetAll();
-    Task<IdentityUser> GetById();
-    Task<User?> GetByName(string userName);
-    Task<IdentityUser> GetByEmail(string email);
-    Task<UserDataChange> UpdateById(string id, UserDataChange userDataChange);
-    Task DeleteUserById(string id);
-    Task<UserDataChange> UpdatePasswordById(string id, UserPasswordChange userPasswordChange);
+    Task<IEnumerable<ApplicationUser>> GetAll();
+    Task<ApplicationUser?> Get(string userName);
+    Task<UserDataChange> Update(string userName, UserDataChange userDataChange);
+    Task DeleteUser(string userName);
+    Task<UserDataChange> UpdatePassword(string userName, UserPasswordChange userPasswordChange);
+    Task<IEnumerable<City>> GetFavoriteCities(string userName);
+    Task<City> AddFavorite(string userName, int cityId);
+    Task DeleteFavorite(string userName, int cityId);
 }
