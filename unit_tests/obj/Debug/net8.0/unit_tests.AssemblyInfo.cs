b0eb4fbfde18dccd5cfd40e0f80395ef31903658
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-02 11:51:40 +0200

    updated frontend config for development out of docker environment

--- unit_tests/obj/Debug/net8.0/unit_tests.AssemblyInfo.cs
+++ unit_tests/obj/Debug/net8.0/unit_tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("unit_tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e975c534e36c26441ed7cf76a91fa863731a444")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4dbd39434a353f1d69aef1158bbf342fbbc676")]
 [assembly: System.Reflection.AssemblyProductAttribute("unit_tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("unit_tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
