47a8e6ba358b15ed6eab302ba7ca0278f3e08995
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-02 12:17:07 +0200

    added attributes to UserDataChange.cs class for validation

--- solar_watch_backend/Models/Contracts/UserDataChange.cs
+++ solar_watch_backend/Models/Contracts/UserDataChange.cs
@@ -1,3 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace solar_watch_backend.Models.Contracts;
 
-public record UserDataChange(string Email, string UserName);
+public partial record UserDataChange
+{
+    [Required]
+    [MinLength(3, ErrorMessage = "Email must be at least 3 characters long.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string Email { get; init; }
+    
+    [Required]
+    [MinLength(3, ErrorMessage = "UserName must be at least 3 characters long.")]
+    public string UserName { get; init; }
+
+    public UserDataChange(string email, string userName)
+    {
+        Email = email;
+        UserName = userName;
+    }
+}
